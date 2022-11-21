14bf505bd6f031c0cec0e58046b1664e6d2ae236
quanghiepdev <hiep@localhost>
2022-11-21 19:23:27 +0700

    fix export environment variable

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ wget https://accellera.org/images/downloads/standards/systemc/systemc-2.3.3.tar.
 tar -xvf systemc-2.3.3.tar.gz
 rm systemc-2.3.3.tar.gz
 cd systemc-2.3.3 &&\
-echo "SYSTEMC_HOME=`pwd`" >> ~/.profile &&\
+echo "export SYSTEMC_HOME=`pwd`" >> ~/.profile &&\
 export CXX=g++ &&\
 mkdir objdir && cd objdir &&\
 ../configure &&\
