b59ab807ee177e1cb3571d10e0d61b7bc129592f
quanghiepdev <hiep@localhost>
2022-11-21 20:05:37 +0700

    clean

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt install tar build-essential -y
 curl https://accellera.org/images/downloads/standards/systemc/systemc-2.3.3.tar.gz -o systemc.tar.gz
 tar -xvf systemc.tar.gz
-rm systemc-2.3.3.tar.gz
+rm systemc.tar.gz
 cd systemc-2.3.3 &&\
 echo "export SYSTEMC_HOME=`pwd`" >> ~/.profile &&\
 export CXX=g++ &&\
