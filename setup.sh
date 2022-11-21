1d21c769234684433560fb14636acce143c79c38
quanghiepdev <hiep@localhost>
2022-11-21 18:58:08 +0700

    add debian distro setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt install tar wget build-essential -y
+wget https://accellera.org/images/downloads/standards/systemc/systemc-2.3.3.tar.gz
+tar -xvf systemc-2.3.3.tar.gz
+rm systemc-2.3.3.tar.gz
+cd systemc-2.3.3 &&\
+echo "SYSTEMC_HOME=`pwd`" >> ~/.profile &&\
+export CXX=g++ &&\
+mkdir objdir && cd objdir &&\
+../configure &&\
+make install
+
